34e25a25688d50f6cece087dd58d5a3952136046
water <2877584094@qq.com>
2024-07-28 22:18:56 +0800

    llama2

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from langchain_core.output_parsers import StrOutputParser
 llm = Ollama(model="llama2")
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a world class technical documentation writer."),
-    ("user", "{input}")
+    ("user", "{how can langsmith help with testing?}")
 ])
 chain = prompt | llm 
 output_parser = StrOutputParser()
